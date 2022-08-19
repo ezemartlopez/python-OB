5267571140c5a62ff77cf59832f3ede079c605da
ezemartlopez <emartinezlopez@est.frba.utn.edu.ar>
2022-08-19 15:57:50 -0300

    modificacion

--- 8_archivos_salidas/ejercicio1.py
+++ 8_archivos_salidas/ejercicio1.py
@@ -1,6 +1,17 @@
-archivo = open("archivo.txt","w")
-
+archivo = open("archivo.txt","w+")
 archivo.write("#un comentario\n")
-archivo.write("unaVariable = 123\n")
-
 archivo.close()
+
+leer = open("archivo.txt","r")
+for linea in leer.readlines():
+    print(linea)
+leer.close()
+print('====================================')
+archivo = open('archivo.txt','a')
+archivo.write("unaVariable = 123\n")
+archivo.close()
+
+leer = open("archivo.txt","r")
+for linea in leer.readlines():
+    print(linea)
+leer.close()
